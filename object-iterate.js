6caf0fa6be8b5aceb92ac8dc1c86de209522d28e
AyamotoKohei <koh14taka2@gmail.com>
2021-11-13 00:01:38 +0900

    add: オブジェクトの反復処理の学習

---
+++ object-iterate.js
@@ -0,0 +1,15 @@
+const user = {
+  id: 3,
+  name: 'Bobby Kumanov',
+  username: 'bobby',
+  email: 'bobby@maple.com',
+};
+
+// プロパティのキーのリスト
+console.log(Object.keys(user));
+
+// プロパティ値のリスト
+console.log(Object.values(user));
+
+// キーと値が対になった 2 次元配列
+console.log(Object.entries(user));
